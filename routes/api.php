7a4e7bbe4218e3a493e8c0c86f7058d3467dc227
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-07 15:49:06 -0400

    Registro de rutas de servicios

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ServiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,4 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // Rutas para clientes y servicios
-Route::apiResource('clients', ClientController::class);
+Route::apiResources([
+    'clients'  => ClientController::class,
+    'services' => ServiceController::class,
+]);
