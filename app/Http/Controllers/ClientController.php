966df9b8b63e9601d448f729b2956556ec0d48b1
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-07 12:52:33 -0400

    Método store terminado y campos fillables dados de alta en el modelo Client

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ClientController extends Controller
 {
@@ -37,7 +38,39 @@ class ClientController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data   = [];
+        $status = 200;
+
+        try {
+            $validator = Validator::make($request->all(), [
+                'name'    => 'required|string|max:255',
+                'email'   => 'required|string|email|unique:clients|max:255',
+                'phone'   => 'nullable|string',
+                'address' => 'nullable|string', // Permite que el campo sea nulo
+            ]);
+
+            if ($validator->fails()) {
+                $data = $validator->errors();
+                $status = 400;
+
+                return response()->json($data, $status);
+            }
+
+            // Validación exitosa
+            $validated = $validator->validated();
+
+            $client = Client::create($validated);
+            $data = [
+                'message' => 'Cliente creado exitosamente',
+                'client'  => $client
+            ];
+        }
+        catch (\Exception $e) {
+            $data = ['error' => $e->getMessage()];
+            $status = 400;
+        }
+
+        return response()->json($data, $status);
     }
 
     /**
--- app/Models/Client.php
+++ app/Models/Client.php
@@ -10,6 +10,18 @@ class Client extends Model
     use HasFactory;
 
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+    ];
+
+    /**
      * The services that belong to the client.
      */
     public function services()
