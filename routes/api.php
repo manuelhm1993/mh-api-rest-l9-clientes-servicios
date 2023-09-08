d7dbcf6da057fb82b5bd052a19b616c47188ea68
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-08 03:26:40 -0400

    Servicios contratados por clientes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ClientController;
-use App\Http\Controllers\ClientServiceController;
 use App\Http\Controllers\ServiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -32,4 +31,9 @@ Route::controller(ClientController::class)->prefix('clients')->name('clients.')-
     Route::get('/services/contracts', 'contracts')->name('services.contracts'); // Cada ruta debe ser específica si se combina
     Route::post('/services/attach', 'attach')->name('services.attach');
     Route::post('/services/detach', 'detach')->name('services.detach');
+});
+
+// Rutas para trabajo de relaciones con servicios y clientes
+Route::controller(ServiceController::class)->prefix('services')->name('services.')->group(function () {
+    Route::get('/clients/contracts', 'contracts')->name('clients.contracts');
 });
